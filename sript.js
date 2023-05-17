6ee318eb719a663fd56801a6204efa5ef38fd944
marialuizacm <marialuiza.mariano@icloud.com>
2023-05-17 09:03:09 -0300

    commit inicial

---
+++ sript.js
@@ -0,0 +1,56 @@
+const input = document.querySelector("input");
+
+const button = document.querySelector("button");
+
+const img = document.querySelector("img");
+
+const city = document.querySelector("#city");
+
+const degree = document.querySelector("#degree");
+
+const content = document.querySelector(".content");
+
+
+
+button.addEventListener("click", async () => {
+
+  if (!input.value) return;
+
+
+
+  try {
+
+    const urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input.value)}&units=metric&appid=YOUR_API_KEY_HERE`;
+
+    const res = await fetch(urlApi);
+
+    const data = await res.json();
+
+
+
+    if (data?.cod && data.code === "404") {
+
+      return alert("Cidade não encontrada.");
+
+    }
+
+
+
+    loadWeatherInfo(data);
+
+  } catch (error) {
+
+    alert(error);
+
+  }
+
+});
+
+
+
+function loadWeatherInfo(data) {
+
+  city.innerHTML = data.name;
+
+}
+
